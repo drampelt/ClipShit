1fe4be076fe2fede4807418b0e872f97dc900bc2
Daniel Rampelt <drampelt@gmail.com>
2015-03-21 13:37:53 -0400

    yolo

---
+++ src/clipshit/Main.java
@@ -0,0 +1,10 @@
+package clipshit;
+
+/**
+ * Created by daniel on 2015-03-21.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ayy lmao");
+    }
+}
