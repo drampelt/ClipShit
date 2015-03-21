78336a679ba4e12f77dc8ca2d64edce03b24dca0
Daniel Rampelt <drampelt@gmail.com>
2015-03-21 17:34:56 -0400

    even more formats

--- src/main/java/clipshit/RandomFormatter.java
+++ src/main/java/clipshit/RandomFormatter.java
@@ -10,29 +10,46 @@ public class RandomFormatter {
     private static Random random = new Random();
     private static String[] formats = {
             "font-weight: bold;",
+            "font-weight: bold;",
+            "text-decoration: underline;",
             "text-decoration: underline;",
             "font-style: italic;",
-            "font-size: 64px;",
-            "letter-spacing: 10px;",
+            "font-style: italic;",
+            "font-style: italic;",
             "text-transform: uppercase",
             "color: #ff0000;",
             "color: #00ff00;",
             "color: #FF40FF;",
-            "line-height: 30px;",
-            "text-indent: 30px;",
             "color: #4E8F00;",
-            "font-size: 128px;",
+            "color: #0096FF;",
+            "color: #941651;",
+            "color: #009192;",
+            "color: #FEFC78;",
+            "line-height: 20px;",
+            "text-indent: 20px;",
             "font-size: 32px;",
-            "font-size: 8px;",
+            "font-size: 64px;",
+            "font-size: 80px;",
+            "font-size: 5px;",
+            "font-size: 10px;",
+            "font-size: 16px;",
+            "font-size: 20px;",
             "font-weight: 100;",
+            "letter-spacing: 10px;",
+            "letter-spacing: 5px;",
             "letter-spacing: -3px;",
+            "letter-spacing: -5px;",
+            "letter-spacing: -1px;",
             "font-family: monospace;",
-            "font-family: \"Comic Sans MS\";"
+            "font-family: 'Papyrus';",
+            "font-family: 'Comic Sans MS';",
+            "opacity: 0.5;",
+            "opacity: 0.7",
     };
 
     public static String format(String in) {
         String html = "<meta charset=\"utf-8\">";
-        String lastOpened = "";
+        int numspans = 0;
         String[] lines = in.split("\n");
         for(String line : lines) {
             float rand = random.nextFloat();
@@ -46,16 +63,19 @@ public class RandomFormatter {
             String[] words = line.split(" ");
             for(String word : words) {
                 rand = random.nextFloat();
-                if(lastOpened != "" && rand > 0.6) {
-                    html += "</" + lastOpened + ">";
-                    lastOpened = "";
+                if(numspans > 0 && rand > 0.6) {
+                    html += "</span>";
+                    numspans--;
                 } else if(rand < 0.6) {
                     html += "<span style=\"" + getStyle() + "\">";
-                    lastOpened = "span";
+                    numspans++;
                 }
                 html += word + " ";
             }
-            if(lastOpened != "") html += "</" + lastOpened + ">";
+            while(numspans > 0) {
+                html += "</span>";
+                numspans--;
+            }
             html += "</p>";
         }
         return html;
