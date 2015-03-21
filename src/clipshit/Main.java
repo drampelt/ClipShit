b9cc363e3589984ddb5bee2d421bf1114fcfd84f
Daniel Rampelt <drampelt@gmail.com>
2015-03-21 13:45:29 -0400

    swag

--- src/clipshit/Main.java
+++ src/clipshit/Main.java
@@ -1,10 +1,28 @@
 package clipshit;
 
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+
 /**
  * Created by daniel on 2015-03-21.
  */
 public class Main {
     public static void main(String[] args) {
         System.out.println("ayy lmao");
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Clipboard clipboard = toolkit.getSystemClipboard();
+        String result = null;
+        try {
+            result = (String) clipboard.getData(DataFlavor.stringFlavor);
+        } catch (UnsupportedFlavorException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(result);
     }
 }
+
